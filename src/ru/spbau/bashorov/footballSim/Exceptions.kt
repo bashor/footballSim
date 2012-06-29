41fcb5b1b96a080d424d50257848c2e9dde3d6a8
Zalim Bashorov <bashor@ya.ru>
2012-06-29 22:36:14 +0400

    Refactoring add test for GameEngine

--- src/ru/spbau/bashorov/footballSim/Exceptions.kt
+++ src/ru/spbau/bashorov/footballSim/Exceptions.kt
@@ -2,4 +2,5 @@ package ru.spbau.bashorov.footballSim
 
 public open class GameInternalException(message: String = "") : RuntimeException(message)
 
+public class IllegalArgumentException(message: String = "") : GameInternalException(message)
 public class BallNotFoundException(message: String = "Ball not found.") : GameInternalException(message)
